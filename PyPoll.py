f9963a35f703fb001aac202d398f86b9901e1cd8
Vishal Singh <vishal.singh@sony.com>
2022-08-30 10:41:02 -0700

    PyPoll 1.0

---
+++ PyPoll.py
@@ -0,0 +1,23 @@
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write three counties to the file.
+outfile.write(" Counties in the Election\n"
+              f"-----------------------------\n\n"
+              f"Arapahoe\nDenver\nJefferson\n")
+
+
+# Close the file
+outfile.close()
